140e2c474ea902cf4f61e4578a54b55c19e0dc93
Mehmedx7 <mehmedkhan16@gmail.com>
2024-03-07 19:45:47 +0530

    model and admin created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Header)
+admin.site.register(Popular_destination)
+admin.site.register(Packages)
+admin.site.register(Photo_gallery)
+admin.site.register(Call_to_action)
+admin.site.register(footer)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Header(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=200)
+    background_image = models.ImageField(upload_to='header_background_img/')
+
+    def __str__(self):
+        return self.title
+    
+class Popular_destination(models.Model):
+    sub_heading = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Popular_destination_img/')
+    city = models.CharField(max_length=200)
+    stars = models.PositiveIntegerField()
+    heading = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.heading
+    
+class Packages(models.Model):
+    sub_heading = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Popular_destination_img/')
+    heading = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    days_nights = models.CharField(max_length=100)
+    pax = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    review_count = models.CharField(max_length=100)
+    stars = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.heading
+    
+class Photo_gallery(models.Model):
+    sub_heading = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='Photo_gallery/')
+
+    def __str__(self):
+        return f'Photos'
+    
+    
+class Call_to_action(models.Model):
+    heading = models.CharField(max_length=100)
+    sub_heading = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.heading
+
+    
+class footer(models.Model):
+    description = models.CharField(max_length=100)
+    date = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.date
+    
+
+    
+
+
+
+
+
+
+    
+
