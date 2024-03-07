4e2803be11ebccb22264a62d8be713c76a2e720b
Mehmedx7 <mehmedkhan16@gmail.com>
2024-03-07 20:13:42 +0530

    content added

--- api/models.py
+++ api/models.py
@@ -4,33 +4,31 @@ from django.db import models
 
 
 class Header(models.Model):
-    title = models.CharField(max_length=100)
-    sub_title = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
+    sub_title = models.CharField(max_length=500)
     background_image = models.ImageField(upload_to='header_background_img/')
 
     def __str__(self):
         return self.title
     
 class Popular_destination(models.Model):
-    sub_heading = models.CharField(max_length=100)
     image = models.ImageField(upload_to='Popular_destination_img/')
-    city = models.CharField(max_length=200)
+    city = models.CharField(max_length=500)
     stars = models.PositiveIntegerField()
-    heading = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    heading = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
 
     def __str__(self):
         return self.heading
     
 class Packages(models.Model):
-    sub_heading = models.CharField(max_length=100)
     image = models.ImageField(upload_to='Popular_destination_img/')
-    heading = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    days_nights = models.CharField(max_length=100)
-    pax = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    review_count = models.CharField(max_length=100)
+    heading = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    days_nights = models.CharField(max_length=500)
+    pax = models.CharField(max_length=500)
+    city = models.CharField(max_length=500)
+    review_count = models.CharField(max_length=500)
     stars = models.PositiveIntegerField()
     price = models.PositiveIntegerField()
 
@@ -38,7 +36,6 @@ class Packages(models.Model):
         return self.heading
     
 class Photo_gallery(models.Model):
-    sub_heading = models.CharField(max_length=100)
     images = models.ImageField(upload_to='Photo_gallery/')
 
     def __str__(self):
@@ -46,15 +43,15 @@ class Photo_gallery(models.Model):
     
     
 class Call_to_action(models.Model):
-    heading = models.CharField(max_length=100)
-    sub_heading = models.CharField(max_length=100)
+    heading = models.CharField(max_length=500)
+    sub_heading = models.CharField(max_length=500)
 
     def __str__(self):
         return self.heading
 
     
 class footer(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
     date = models.CharField(max_length=10)
 
     def __str__(self):
